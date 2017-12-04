aab2d32fe6851607b95048945d4f39b78d9b55cf
Ramon Iglesias <rdit@stanford.edu>
2017-12-04 12:38:55 -0800

    s

--- models.py
+++ models.py
@@ -80,9 +80,11 @@ def rotation_model(inputs, train=True, norm=True, **kwargs):
     # rotations
     rotation_labels = np.random.randint(0, 4, batch_size,dtype='int32')
     print inputs['images'].get_shape().as_list(), rotation_labels.shape
-    input_to_network = tf.map_fn(lambda x: tf.image.rot90(x[0], x[1]), 
+    input_to_network = tf.map_fn(
+        lambda x: tf.image.rot90(x[0], x[1]), 
         (inputs['images'], rotation_labels), 
-        dtype=(tf.float32, tf.int32))
+        dtype=tf.float32
+        )
     outputs['labels_rotation'] = rotation_labels
     ### YOUR CODE HERE
 
