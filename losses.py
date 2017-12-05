49617ec74aa46a02f691763e23c0618af3ca4bb3
Ramon Iglesias <rdit@stanford.edu>
2017-12-04 22:04:55 -0800

    regularizer

--- losses.py
+++ losses.py
@@ -5,14 +5,19 @@ import tensorflow as tf
 def classification_loss(inputs, outputs):
     labels = outputs['labels']
     logits = outputs['pred']
-    return tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits)
+    clf_loss =  tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits)
+    reg_loss = tf.add_n(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES))
+    return clf_loss + reg_loss
  
 def rotation_loss(inputs, outputs):
     labels = outputs['labels_rotation']
     logits = outputs['pred_rotation']
-    return tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits)
+    rot_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits)
+    reg_loss = tf.add_n(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES))
+    return rot_loss + reg_loss
 
 def multitask_loss(inputs, outputs):
     clf_loss = classification_loss(inputs,outputs)
     rot_loss = rotation_loss(intpus, outputs)
-    return clf_loss + rot_loss
+    reg_loss = tf.add_n(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES))
+    return clf_loss + rot_loss + reg_loss
